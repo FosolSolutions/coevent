e62b847da797381fb05080d82af61c65aa655b13
Jeremy Foster <jeremymfoster@hotmail.com>
2021-12-30 11:42:03 -0800

    Add FormikAutoComplete component

---
+++ app/src/components/form/formik-auto-complete/FormikAutoComplete.tsx
@@ -0,0 +1,44 @@
+import { useFormikContext } from 'formik';
+
+import { AutoComplete, IAutoCompleteProps } from '..';
+import * as styled from './FormikAutoCompleteStyled';
+
+export interface IFormikAutoCompleteProps extends IAutoCompleteProps {
+  name: string;
+  label?: string;
+  value?: string | number | readonly string[];
+}
+
+export const FormikAutoComplete = <T extends any>({
+  id,
+  name,
+  label,
+  value,
+  onChange,
+  onBlur,
+  children,
+  className,
+  ...rest
+}: IFormikAutoCompleteProps) => {
+  const { values, errors, touched, handleBlur, handleChange } = useFormikContext<T>();
+  const error = (errors as any)[name] && (touched as any)[name] && (errors as any)[name];
+  return (
+    <styled.FormikAutoComplete>
+      <label htmlFor={`dpn-${name}`}>{label}</label>
+      <div>
+        <AutoComplete
+          id={id ?? `dpn-${name}`}
+          name={name}
+          value={value ?? (values as any)[name] ?? ''}
+          onChange={onChange ?? handleChange}
+          onBlur={onBlur ?? handleBlur}
+          className={error ? `${className} error` : className}
+          {...rest}
+        >
+          {children}
+        </AutoComplete>
+        {error ? <p role="alert">{error}</p> : null}
+      </div>
+    </styled.FormikAutoComplete>
+  );
+};
